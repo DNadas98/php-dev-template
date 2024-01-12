fa58395604de239facb757b657281ffa6c30b1c9
DNadas98 <daniel.nadas.123@gmail.com>
2024-01-12 22:54:47 +0100

    Add basic example code to index.php

--- public/index.php
+++ public/index.php
@@ -5,37 +5,78 @@ use PhpStarter\Model\ExampleRepository;
 
 $repository = new ExampleRepository();
 
-echo "Testing create...\n";
-$repository->create('Test1');
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['action'])) {
+    $id = $_POST['id'] ?? null;
+    $name = $_POST['name'] ?? '';
 
-echo "Testing readAll...\n";
+    switch ($_POST['action']) {
+        case 'create':
+            $repository->create($name);
+            break;
+        case 'update':
+            $repository->update($id, $name);
+            break;
+        case 'delete':
+            $repository->delete($id);
+            break;
+    }
+
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit();
+}
+
 $allExamples = $repository->readAll();
-foreach ($allExamples as $example) {
-    echo "ID: " . $example->getId() . " Name: " . $example->getName() . "\n";
-}
+?>
 
-echo "Testing readById...\n";
-$example = $repository->readById(1);
-if ($example) {
-    echo "ID: " . $example->getId() . " Name: " . $example->getName() . "\n";
-} else {
-    echo "No example found.\n";
-}
-
-echo "Testing update...\n";
-$repository->update(1, 'Test1-updated');
-$updated = $repository->readById(1);
-if ($updated) {
-    echo "ID: " . $updated->getId() . " Name: " . $updated->getName() . "\n";
-} else {
-    echo "No example found.\n";
-}
-
-echo "Testing delete...\n";
-$repository->delete(1);
-$deleted = $repository->readById(1);
-if ($deleted) {
-    echo "ID: " . $deleted->getId() . " Name: " . $deleted->getName() . "\n";
-} else {
-    echo "No example found.\n";
-}
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+    <title>Example List</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        body{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            width: 100%;
+            font-family: monospace;
+            font-size: 1rem;
+        }
+        li {
+            width: 100%;
+            display: flex;
+            flex-direction: row;
+            gap: 0.3rem;
+            padding-bottom: 0.3rem;
+        }
+    </style>
+</head>
+<body>
+<h1>Example List</h1>
+<ul>
+    <li>
+        <form action="index.php" method="post">
+            <input type="text" name="name" minlength="1" required placeholder="Name"/>
+            <input type="hidden" name="action" value="create"/>
+            <button type="submit">+</button>
+        </form>
+    </li>
+    <?php foreach ($allExamples as $example): ?>
+        <li>
+            <form action="index.php" method="post">
+                <input type="text" name="name" value="<?= htmlspecialchars($example->getName()) ?>"/>
+                <input type="hidden" name="id" value="<?= $example->getId() ?>"/>
+                <input type="hidden" name="action" value="update"/>
+                <button type="submit">Save</button>
+            </form>
+            <form action="index.php" method="post">
+                <input type="hidden" name="id" value="<?= $example->getId() ?>"/>
+                <input type="hidden" name="action" value="delete"/>
+                <button type="submit">X</button>
+            </form>
+        </li>
+    <?php endforeach; ?>
+</ul>
+</body>
+</html>
